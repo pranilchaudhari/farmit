3c269e7feccc05fe369c82f1b59ab8419c9d4311
pranilchaudhari <44390723+pranilchaudhari@users.noreply.github.com>
2019-02-23 11:59:28 +0530

    Add files via upload

---
+++ bagPranil/js/custom.js
@@ -0,0 +1,13 @@
+// Add bx slider to screens
+$(document).ready(function() {
+    
+    $('.bxslider').bxSlider({
+        
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1, 
+        maxSlides: 1, 
+        slideMargin: 50
+    });
+    
+});
